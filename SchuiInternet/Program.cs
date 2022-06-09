fab3a37110603a9e899f20a52c845f989b48cac3
Jonas Kellermayr <jonas.kellermayr@gmail.com>
2022-06-09 12:47:44 +0200

    config file geht oiss, connecten geht imma nu

--- SchuiInternet/Program.cs
+++ SchuiInternet/Program.cs
@@ -4,40 +4,93 @@ public class Program
 {
     private const string URL = "http://10.10.0.251:8002/index.php?zone=cp_htl";
 
-    private readonly string username;
-    private readonly string password;
+    private string username;
+    private string password;
+
+    private readonly string filePath;
 
     public static void Main(string[] args) => new Program().Init();
 
     public Program()
     {
         string winUser = Environment.UserName;
+        filePath = $@"C:\Users\{winUser}\.conconfig";
 
         try
         {
-            string[] fileText = File.ReadAllLines($@"C:\Users\{winUser}\.conconfig.con");
+            string[] fileText = File.ReadAllLines(filePath);
 
             username = fileText[0];
             password = fileText[1];
         }
-        catch
+        catch (Exception)
         {
-            Console.WriteLine("Error");
-            Environment.Exit(1);
+            Console.WriteLine("Irgendwos passt an deim file ned");
+
+            Console.WriteLine("Username:");
+            username = Console.ReadLine();
+
+            Console.WriteLine("Password:");
+            password = Console.ReadLine();
+
+
+            ReConfigure();
         }
     }
 
     private void Init()
     {
+        Console.WriteLine("To see list of availabe commands, type 'IAmAUselessMemberOfSocietyAndRequireALobotomyMarker'");
+
         while (true)
         {
+            Console.WriteLine();
             string input = Console.ReadLine();
 
-            if (input == "connect")
+            switch (input)
             {
-                Connect();
+                case "connect":
+                    Connect();
+                    break;
+                case "reconfig":
+                    ReConfigure();
+                    break;
+                case "IAmAUselessMemberOfSocietyAndRequireALobotomyMarker":
+                case "help":
+                    DisplayCommands();
+                    break;
             }
         }
+    }
+
+    private void DisplayCommands()
+    {
+        Console.WriteLine("connect: try to connect with saved credentials");
+        Console.WriteLine("reconfig: re-enter credentials\n");
+    }
+    
+    private void ReConfigure()
+    {
+        File.Delete(filePath);
+        File.Create(filePath).Close();
+
+        Console.WriteLine("Username:");
+        username = Console.ReadLine();
+
+        Console.WriteLine("Password:");
+        password = Console.ReadLine();
+
+        if (username == null || username == "" || password == null || password == "")
+        {
+            Console.WriteLine("konnst du wortwörtlich kan string eigebn?");
+            Console.WriteLine("kumm geh scheißn und moch da dei config file söwa (oda start afoch donwengung neich)");
+
+            Environment.Exit(1);
+        }
+
+        File.WriteAllLines(filePath, new string[] { username, password });
+
+        Console.WriteLine("perfekt, fetzt");
     }
 
     private bool Connect()
