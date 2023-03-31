7833e84835b39d735809ddb4fa47d86d53822275
Jonas Kellermayr <jonas.kellermayr@gmail.com>
2023-03-31 11:02:33 +0200

    deafad jetzt moi fertig sei, rip python (again)

--- SchuiInternet/Program.cs
+++ SchuiInternet/Program.cs
@@ -1,63 +1,43 @@
 ﻿namespace htlgkr;
 
 public class Program {
-    #region Vars
-    private const string URL = "http://10.10.0.251:8002/index.php?zone=cp_htl";
-    private const string REDIURL = "http://10.10.0.2/captiveportal/cp_logon_done.html";
+    public static void Main(string[] args)
+        => new HTLGKR().Init();
+}
+
+public class HTLGKR {
+    public const string URL = "http://10.10.0.251:8002/index.php?zone=cp_htl";
+    public const string REDIURL = "http://10.10.0.2/captiveportal/cp_logon_done.html";
+
+    public readonly string dirPath;
+    public readonly string filePath;
 
     private string username;
     private string password;
 
-    private readonly string filePath;
+    public HTLGKR() {
+        dirPath = @"C:\ProgramData\htlgkr";
+        filePath = dirPath + @"\.htl";
+    }
 
-    private readonly Dictionary<Command, Action> commands;
-    #endregion
-
-    #region Ctor
-    public static void Main(string[] args)
-        => new Program().Init(args);
-
-    public Program() {
-        commands = new() {
-            [Command.connect] = Connect,
-            [Command.config] = ReConfigure,
-        };
-
-        string winUser = Environment.UserName;
-        filePath = $@"C:\Users\{winUser}\.htl";
-
+    public void Init() {
         try {
+            Directory.CreateDirectory(dirPath);
+
             string[] fileText = File.ReadAllLines(filePath);
 
             username = fileText[0];
             password = fileText[1];
         }
         catch (Exception) {
-            Console.WriteLine("Irgendwos passt an deim file ned");
+            Console.WriteLine("Invalid credentials\n");
 
             ReConfigure();
         }
+
+        Connect();
     }
 
-    private void Init(string[] args) {
-        if (args.Length == 0) {
-            Connect();
-            return;
-        }
-
-        bool parsed = Enum.TryParse(args[0], out Command c);
-
-        if (!parsed) {
-            Console.WriteLine("Invalid command");
-            return;
-        }
-        else {
-            commands[c].Invoke();
-        }
-    }
-    #endregion
-
-    #region ConsoleFunctions
     public void ReConfigure() {
         Console.WriteLine("Username:");
         username = Console.ReadLine();
@@ -77,23 +57,17 @@ public class Program {
         File.WriteAllLines(filePath, new string[] { username, password });
     }
 
-    private string GetObscuredPassword() {
+    private static string GetObscuredPassword() {
         string pass = "";
+        ConsoleKeyInfo key = Console.ReadKey(true);
 
-        ConsoleKey key;
         do {
-            key = Console.ReadKey(true).Key;
+            pass += key.KeyChar;
+            Console.Write("*");
 
-            if (key == ConsoleKey.Backspace & pass.Length > 0) {
-                Console.Write("\b \b");
-                pass = pass[0..^1];
-                continue;
-            }
-            else if (!char.IsControl(Console.ReadKey(true).KeyChar)) {
-                Console.Write("*");
-                pass += key;
-            }
-        } while (key != ConsoleKey.Enter);
+            key = Console.ReadKey(true);
+        }
+        while (key.Key != ConsoleKey.Enter);
 
         return pass;
     }
@@ -115,25 +89,19 @@ public class Program {
 
             string page = reply.Content.ReadAsStringAsync().Result;
 
-            if (page == "You are connected.") {
-                Console.WriteLine("Already connected");
-                return;
-            }
-
-            if (page.Contains("erfolgreich")) {
+            if (page == "You are connected." && page.Contains("erfolgreich")) {
                 Console.WriteLine("Connected");
                 return;
             }
         }
 
-        Console.WriteLine("fetzt ned");
+        Console.WriteLine("Connection attempt failed");
 
-        Console.Write("Passwort numoi eigebn? [Y|n]");
+        Console.WriteLine("Re-enter credentials? [Y|n]");
 
-        string input = Console.ReadLine();
+        string input = GetObscuredPassword();
 
         if (input == "" || input.ToUpperInvariant() == "Y")
             ReConfigure();
     }
-    #endregion
 }
